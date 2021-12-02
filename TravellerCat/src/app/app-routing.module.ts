1dda84ed63f45132a44398473a56cf4fb171ecdf
Angeltrek <angeltrek007@gmail.com>
2021-12-01 20:37:54 -0600

    Hecho hasta inicio y continentes

--- TravellerCat/src/app/app-routing.module.ts
+++ TravellerCat/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContinentesComponent } from './continentes/continentes.component';
+import { GaleriaComponent } from './galeria/galeria.component';
+import { InicioComponent } from './inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent},
+  { path: 'continentes', component: ContinentesComponent},
+  { path: 'galeria', component: GaleriaComponent},
+  { path: 'about', component: AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- TravellerCat/src/app/app.module.ts
+++ TravellerCat/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ContinentesComponent } from './continentes/continentes.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContinentesComponent,
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
